3b18d6e5607e311b12ffac30bbd57c466d5a079b
NectarSpot <nectarspot@gmail.com>
2023-07-28 16:55:10 +0530

    Add files via upload

---
+++ Design/multi-language/multi-lan.js
@@ -0,0 +1,3 @@
+< script > window.gtranslateSettings = { "default_language": "en", "native_language_names": true, "languages": ["en", "es", "hi"], "wrapper_selector": ".gtranslate_wrapper", "switcher_horizontal_position": "right", "switcher_vertical_position": "top", "alt_flags": { "en": "usa" } } < /script> <
+    script src = "https://cdn.gtranslate.net/widgets/latest/dwf.js"
+defer > < /script>
